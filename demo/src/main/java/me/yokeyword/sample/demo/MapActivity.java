bebfb1d33d516b3a8038d89b31fe952d180c98f0
zhaoxuyan <526161421@qq.com>
2017-12-06 11:48:28 +0800

    :hospital:校医院Marker

--- demo/src/main/java/me/yokeyword/sample/demo/MapActivity.java
+++ demo/src/main/java/me/yokeyword/sample/demo/MapActivity.java
@@ -7,8 +7,12 @@ import android.widget.Toast;
 
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.map.TextureMapView;
 import com.baidu.mapapi.model.LatLng;
 
@@ -22,7 +26,7 @@ import me.yokeyword.sample.demo.helper.NetWorkUtils;
 
 public class MapActivity extends AppCompatActivity {
     private TextureMapView mMapView;
-    private BaiduMap baiduMap;
+    private BaiduMap mBaiduMap;
     private MapStatusUpdate msu;
 
     @Override
@@ -42,13 +46,56 @@ public class MapActivity extends AppCompatActivity {
         }
         setContentView(R.layout.activity_map);
         mMapView = (TextureMapView) findViewById(R.id.bmapView);
-        baiduMap = mMapView.getMap();
+        mBaiduMap = mMapView.getMap();
+
+        //构建MarkerOption，用于在地图上添加Marker
+        LatLng hospital_point = new LatLng(29.4937262027, 106.5779192736);
+
         //msu = MapStatusUpdateFactory.newLatLng(new LatLng(29.495801,106.576364));
-        msu = MapStatusUpdateFactory.newLatLngZoom(new LatLng(29.495801, 106.576364), 19);
+        msu = MapStatusUpdateFactory.newLatLngZoom(hospital_point, 20);
         // baiduMap.setMapStatus(MapStatusUpdateFactory.newMapStatus(new MapStatus.Builder().zoom(18).build()));//设置缩放级别
 
-        baiduMap.setMapStatus(msu);
+        mBaiduMap.setMapStatus(msu);
+        BitmapDescriptor bitmap = BitmapDescriptorFactory
+                .fromResource(R.drawable.ic_map_hospital);
+
+        OverlayOptions option = new MarkerOptions()
+                .position(hospital_point)
+                .icon(bitmap);
+
+        //在地图上添加Marker，并显示
+
+        mBaiduMap.addOverlay(option);
+
         //logNetWorkInfo();
+
+//        // 自定义定位模式、定位图标、精度圈颜色
+//        MyLocationConfiguration.LocationMode mCurrentMode = MyLocationConfiguration.LocationMode.NORMAL;
+//        BitmapDescriptor mCurrentMarker = BitmapDescriptorFactory.fromResource(R.drawable.ic_map_locate);
+//        int accuracyCircleFillColor = 0xAAFFFF88;
+//        int accuracyCircleStrokeColor = 0xAA00FF00;
+//        mBaiduMap.setMyLocationConfiguration(new MyLocationConfiguration(
+//                mCurrentMode, true, mCurrentMarker,
+//                accuracyCircleFillColor, accuracyCircleStrokeColor));
+//
+//        // 构造定位数据
+//        MyLocationData locData = new MyLocationData.Builder()
+//                .accuracy(location.getRadius())
+//                // 此处设置开发者获取到的方向信息，顺时针0-360
+//                .direction(100).latitude(location.getLatitude())
+//                .longitude(location.getLongitude()).build();
+//
+//        // 设置定位数据
+//        mBaiduMap.setMyLocationData(locData);
+//
+//        // 设置定位图层的配置（定位模式，是否允许方向信息，用户自定义定位图标）
+//        mCurrentMarker = BitmapDescriptorFactory
+//                .fromResource(R.drawable.ic_map_locate);
+//        MyLocationConfiguration config = new MyLocationConfiguration(mCurrentMode, true, mCurrentMarker);
+//        mBaiduMap.setMyLocationConfiguration();
+//
+//        // 当不需要定位图层时关闭定位图层
+//        mBaiduMap.setMyLocationEnabled(false);
 
     }
 
